432dde9c26358aa6f3f4f7e056d1c36994c661f1
Richard Crippen <richard.crippen@gmail.com>
2021-04-27 19:14:41 -0400

    setting up server and made empty routing files

--- server.js
+++ server.js
@@ -2,10 +2,22 @@
 
 const fs = require('fs');
 const path = require('path');
-const express = require('express')
+const express = require('express');
 
-app.get('/', (req, res) => {
-    res.send('Welcome')
+const PORT = process.env.PORT || 8080;
+// const dbJson = require ('.db/db.json');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
   })
    
-  app.listen(3000)
+  app.listen(PORT, function (){
+      console.log ('Listening')
+  });
+
